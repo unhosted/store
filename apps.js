595065eb800967733c7323dc2c90a12d8a462952
Michiel@unhosted <michiel@unhosted.org>
2014-07-07 10:18:51 +0200

    defaultApps and add instructions

--- apps.js
+++ apps.js
@@ -1,42 +1,43 @@
 RemoteStorage.defineModule('apps', function(privClient, pubClient) {
-  var apps = {
-    litewrite: {
-      href: 'http://litewrite.net', // <-- optional; defaults to https://name.5apps.com/ (lowercased)
-      img: '/img/litewrite.png', // <-- optional; defaults to /img/name.png (lowercased)
-      name: 'litewrite' // <-- optional; defaults to name
-    },
-    Laverna: { href: 'https://laverna.cc/' },
-    mcnotes: { },
-    //(no rs) StackEdit: { href: 'https://stackedit.io/' },
-    gHost: { },
-    dogfeed: { },
-    dogtalk: { },
-    // sharesome: { img: 'none' },
-    grouptabs: { },
-    byoDB: { href: 'http://diafygi.github.io/byoDB/examples/diary/' },
-    'time tracker': { href: 'http://shybyte.github.io/unhosted-time-tracker/', img: '/img/time.png' },
-    'svg-edit': { href: 'https://svg-edit.5apps.com/editor/svg-editor.html'},
-    browser: { href: 'https://remotestorage-browser.5apps.com/' },
-    vidmarks: { },
-    music: { href: 'https://music-michiel.5apps.com/' },
-    drinks: { href: 'https://myfavoritedrinks.5apps.com/' },
-    todo: { href: 'https://todomvc.5apps.com/labs/architecture-examples/remotestorage/' },
-    'dspace-client': { href: 'https://dspace-nilclass.5apps.com/', img: '/img/dspace.png' },
-    strut: { href: 'http://tantaman.github.com/Strut/' },
-    //(no rs) TiddlyWiki: { href: 'http://www.tiddlywiki.com/' },
-    //(no rs) Fargo: { href: 'http://fargo.io/' },
-    // Crypton: { href: 'http://crypton.io/', img: 'none' },
-    // Dillinger: { href: 'http://dillinger.io/', img: 'none' },
-    // 'freedom js': { href: 'http://freedomjs.org/', img: 'none' },
-    // 'Peer CDN': { href: 'https://peercdn.com/', img: 'none' },
-    // '+PeerServer': { href: 'http://www.peer-server.com/', img: 'none' },
-    // Mylar: { href: 'http://css.csail.mit.edu/mylar/#Software', img: 'none' },
-    // CryptoSphere: { href: 'http://cryptosphere.org/', img: 'none' },
-    // editor: { href: 'https://editor-michiel.5apps.com/' },
-    // social: { href: 'https://social-michiel.5apps.com/' },
-    // email: { href: 'https://email-michiel.5apps.com/' },
-    // smarkers: { href: 'https://smarker-nilclass.5apps.com/' },
-  };
+  var apps = {},
+    defaultApps = {
+      litewrite: {
+        href: 'http://litewrite.net', // <-- optional; defaults to https://name.5apps.com/ (lowercased)
+        img: '/img/litewrite.png', // <-- optional; defaults to /img/name.png (lowercased)
+        name: 'litewrite' // <-- optional; defaults to name
+      },
+      Laverna: { href: 'https://laverna.cc/' },
+      mcnotes: { },
+      //(no rs) StackEdit: { href: 'https://stackedit.io/' },
+      gHost: { },
+      dogfeed: { },
+      dogtalk: { },
+      // sharesome: { img: 'none' },
+      grouptabs: { },
+      byoDB: { href: 'http://diafygi.github.io/byoDB/examples/diary/' },
+      'time tracker': { href: 'http://shybyte.github.io/unhosted-time-tracker/', img: '/img/time.png' },
+      'svg-edit': { href: 'https://svg-edit.5apps.com/editor/svg-editor.html'},
+      browser: { href: 'https://remotestorage-browser.5apps.com/' },
+      vidmarks: { },
+      music: { href: 'https://music-michiel.5apps.com/' },
+      drinks: { href: 'https://myfavoritedrinks.5apps.com/' },
+      todo: { href: 'https://todomvc.5apps.com/labs/architecture-examples/remotestorage/' },
+      'dspace-client': { href: 'https://dspace-nilclass.5apps.com/', img: '/img/dspace.png' },
+      strut: { href: 'http://tantaman.github.com/Strut/' },
+      //(no rs) TiddlyWiki: { href: 'http://www.tiddlywiki.com/' },
+      //(no rs) Fargo: { href: 'http://fargo.io/' },
+      // Crypton: { href: 'http://crypton.io/', img: 'none' },
+      // Dillinger: { href: 'http://dillinger.io/', img: 'none' },
+      // 'freedom js': { href: 'http://freedomjs.org/', img: 'none' },
+      // 'Peer CDN': { href: 'https://peercdn.com/', img: 'none' },
+      // '+PeerServer': { href: 'http://www.peer-server.com/', img: 'none' },
+      // Mylar: { href: 'http://css.csail.mit.edu/mylar/#Software', img: 'none' },
+      // CryptoSphere: { href: 'http://cryptosphere.org/', img: 'none' },
+      // editor: { href: 'https://editor-michiel.5apps.com/' },
+      // social: { href: 'https://social-michiel.5apps.com/' },
+      // email: { href: 'https://email-michiel.5apps.com/' },
+      // smarkers: { href: 'https://smarker-nilclass.5apps.com/' },
+    };
 
   function fillInBlanks(key, obj) {
     obj.href = obj.href || 'https://'+key.toLowerCase()+'.5apps.com/';
@@ -44,11 +45,11 @@ RemoteStorage.defineModule('apps', function(privClient, pubClient) {
     obj.name = obj.name || key;
     return obj;
   }
-  function addApp(name, obj) {
-    app[name] = obj;
+  function installApp(name) {
+    app[name] = defaultApps[name];
     privClient.storeObject('app', name, obj);
   }
-  function removeApp(name) {
+  function uninstallApp(name) {
     delete apps[name];
     privClient.remove(name);
   }
@@ -80,17 +81,31 @@ RemoteStorage.defineModule('apps', function(privClient, pubClient) {
       apps[evt.relativePath] = evt.newValue;
       changeHandler(apps);
     });
-    for (var name in apps) {
-      privClient.storeObject('app', name, fillInBlanks(name, apps[name]));
-    }
   }
+  
+  function getInstalledApps() {
+    return apps;
+  }
+ 
+  function getAvailableApps() {
+    var i, availableApps = {};
+    for (i in defaultApps) {
+      if (!apps[i]) {
+        availableApps[i] = defaultApps[i];
+      }
+    } 
+    return availableApps;
+  }
+
   //...
   init();
   return {
     exports: {
       onChange: onChange,
-      addApp: addApp,
-      removeApp: removeApp
+      installApp: installApp,
+      uninstallApp: uninstallApp,
+      getInstalledApps: function() { return apps; },
+      getAvailableApps: getAvailableApps
     }
   };
 });
